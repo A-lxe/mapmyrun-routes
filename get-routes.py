d95aa165125932839ebd89073332c612149ecdbc
Alex Aubuchon <alex@alxe.co>
2016-12-01 14:30:43 -0500

    Working! Now saves folders with json metadata and gpx route files in ./out/

--- get-routes.py
+++ get-routes.py
@@ -1,12 +1,26 @@
 import sys
+import os
+import time
 import json
 import pprint
 import requests
+
+def writeRouteFiles(jsonMeta, textGpx, dir='./out'):
+    dir = dir + '/' + jsonMeta['_links']['self'][0]['id']
+    if not os.path.exists(dir): os.makedirs(dir)
+    pp = pprint.PrettyPrinter(indent=4)
+    jsonFile = open(dir + '/meta.json', 'w')
+    jsonFile.write(pp.pformat(jsonMeta))
+    jsonFile.close()
+    gpxFile = open(dir + '/route.gpx', 'w')
+    gpxFile.write(textGpx)
+    gpxFile.close()
 
 url = 'https://oauth2-api.mapmyapi.com'
 
 key = sys.argv[1]
 secret = sys.argv[2]
+limit = int(sys.argv[5])
 
 query = dict()
 query['close_to_location'] = sys.argv[3]
@@ -17,16 +31,23 @@ headers['api-key'] = key
 headers['authorization'] = 'Bearer ' + secret
 headers['content-type'] = 'application/json'
 
-req = requests.get(url + '/v7.1/route/',
-                   params=query,
-                   headers=headers)
+i = 0
+while(limit == -1 or i < limit):
+    print 'Offset: ' + str(i)
+    time.sleep(1)
+    query['offset'] = i
+    req = requests.get(url + '/v7.1/route/',
+                       params=query,
+                       headers=headers)
+    reqJson = req.json()
+    if i >= reqJson['total_count']: break
+    for route in reqJson['_embedded']['routes']:
+        i += 1
+        routeUrl = route['_links']['alternate'][1]['href']
+        routeReq = requests.get(url + routeUrl,
+                                headers=headers)
+        print routeReq.reason
+        print "Writing route " + route['_links']['self'][0]['id']
+        writeRouteFiles(route, routeReq.text)
 
-tmp = req.json()['_embedded']['routes'][0]['_links']['alternate'][1]['href']
-req2 = requests.get(url + tmp,
-                    headers=headers)
-
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(req2.text)
-
-print req.status_code
-print req.reason
+print "Complete!"
